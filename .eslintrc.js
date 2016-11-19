82ce0a77b71395ad4f463b8c92ff5a36f2916840
Dazzy Ding <h@dazzyd.org>
2016-11-19 21:22:55 +0800

    Add babel & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  'installedESLint': true,
+  'parser': 'babel-eslint',
+  'plugins': [
+    'import',
+    'react',
+  ],
+  'globals': {
+    'html2canvas': false,
+  },
+  'rules': {
+    'comma-dangle': ['error', 'always-multiline'],
+    'indent': ['warn', 2],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': ['warn', {'allow': ['warn', 'error']}],
+    'no-var': 'error',
+    'no-unused-vars': ['warn', {'args': 'none'}],
+    'semi': ['error', 'never'],
+    'unicode-bom': 'error',
+    'react/prop-types': 'off',
+  },
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.es', '.coffee', '.cjsx'],
+        'paths': [__dirname],
+      },
+    },
+    'import/core-modules': [
+      'bluebird',
+      'electron',
+      'react',
+      'react-redux',
+      'redux-observers',
+      'reselect',
+      'react-bootstrap',
+      'react-fontawesome',
+      'path-extra',
+      'fs-extra',
+      'lodash',
+      'cson',
+    ],
+  },
+}
